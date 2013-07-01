ca671752638bfc25618a01ff639887c47b5c27db
Jared Forsyth <jared@jaredforsyth.com>
2013-07-01 09:37:48 -0600

    changes for new settings api + reset button

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
 var panel = require('todolist-panel')
-  , settings = require('settings')
+  , settings = require('settings')()
   , chromeStorage = require('settings-chrome-storage');
 
-settings.set('ffapi:main.ffhome', 'https://familyfound.herokuapp.com/');
+settings.set('ffapi.ffhome', 'https://familyfound.herokuapp.com/');
 
 chromeStorage.load(settings, function () {
   panel.attach(window);
--- settings/index.js
+++ settings/index.js
@@ -1,20 +1,20 @@
 
 var panel = require('todolist-panel')
-  , settings = require('settings')
+  , settings = require('settings')()
   , angularSettings = require('angular-settings')
   , chromeStorage = require('settings-chrome-storage');
 
-angularSettings.config('familyfound', {
-  name: 'default',
-  pages: [{
-    module: 'panel',
-    page: 'display'
+angularSettings.config('familyfound', 'default', {
+    settings: ['panel.display.*'],
+    name: 'display',
     title: 'Display'
   }, {
-    module: 'ffapi',
-    page: 'main',
+    settings: ['ffapi.*'],
+    name: 'network',
     title: 'Network'
-  }]
-});
+  });
 
 module.exports = angular.module('ff-settings', ['todolist-panel', 'settings-chrome-storage', 'settings']);
+module.exports.settings = settings;
+
+settings.set('ffapi.ffhome', 'https://familyfound.herokuapp.com/');
